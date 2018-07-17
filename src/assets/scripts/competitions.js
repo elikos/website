a73928c0e927cc9d68e18405b9108e77acb79586
Geoffrey Leclaire <geoffrey.leclaire@polymtl.ca>
2018-07-16 22:23:05 -0400

    Working navigation for competition years

--- src/assets/scripts/competitions.js
+++ src/assets/scripts/competitions.js
@@ -1,9 +1,25 @@
-(function($) {
-    "use strict"; // Start of use strict
-  
-    $('#competition-2017-nav').click(() => {
-        alert('test');
+$(function() {
+    
+
+    const competitionYears = [2017, 2015, 2016];
+    const competitionDivs = Array.from(competitionYears, year => $('#competition-' + year));
+    const competitionNavs = Array.from(competitionYears, year => $('#competition-' + year + '-nav'));
+
+    // Nav click callbacks
+    competitionNavs.forEach(nav => {
+        nav.click(event => {
+            competitionDivs.forEach(div => div.hide());
+            const navId = event.currentTarget.id;
+            const competitionId = navId.substring(0, navId.indexOf('-nav'));
+            $('#' + competitionId).show();
+            
+            competitionNavs.forEach(nav => nav.removeClass('active'));
+            $(event.currentTarget).addClass('active');
+        });
     });
+
+    // Show first div only
+    competitionDivs.forEach(div => div.hide());
+    competitionDivs[0].show();
   
-})(jQuery); // End of use strict
-  
+});
