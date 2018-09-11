2c73e5b63a32a93c4b6bf50ce0e99c7c332840a6
Geoffrey Leclaire <geoffrey.leclaire@polymtl.ca>
2018-09-10 23:14:22 -0400

    Started team section navigation

--- src/assets/scripts/competitions.js
+++ src/assets/scripts/competitions.js
@@ -1,12 +1,6 @@
 $(function() {
-    const competitionYears = [2017, 2015, 2016];
-    const competitionDivs = Array.from(competitionYears, year => $('#competition-' + year));
-    const competitionNavs = Array.from(competitionYears, year => $('#competition-' + year + '-nav'));
-
-    // Competitions nav callbacks
-    competitionNavs.forEach(nav => {
-        nav.click(event => navClickTansition(event, competitionNavs, competitionDivs));
-    }); 
+    initTransitions("competition");
+    initTransitions("team");
 });
 
 function navClickTansition(event, navs, contentDivs) {
@@ -15,20 +9,24 @@ function navClickTansition(event, navs, contentDivs) {
     navs.forEach(nav => nav.removeClass('active'));
     $(event.currentTarget).addClass('active');
 
+    // Animate content divs
     contentDivs.forEach(div => {
         const divYear = div.attr('id').split('-')[1];
+        // Right of clicked year: move to the left
         if (divYear > year) {
             div.animate({
                 left: '-200%',
                 opacity: '0'
             }, 500);
             div.hide();
+        // Left of clicked year: move to the right
         } else if (divYear < year) {
             div.animate({
                 left: '200%',
                 opacity: '0'
             }, 500);
             div.hide();
+        // Clicked year: center
         } else if (divYear === year) {
             div.show();
             div.animate({
@@ -36,5 +34,16 @@ function navClickTansition(event, navs, contentDivs) {
                 opacity: '1'
             }, 500);
         }
+    });
+}
+
+function initTransitions(keyword) {
+    const years = [2017, 2015, 2016];
+    const contentDivs = Array.from(years, year => $('#' + keyword + '-' + year));
+    const navs = Array.from(years, year => $('#' + keyword + '-' + year + '-nav'));
+
+    // Competitions nav callbacks
+    navs.forEach(nav => {
+        nav.click(event => navClickTansition(event, navs, contentDivs));
     });
 }
