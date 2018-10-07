5cc3f19b018f84556bb78176751d00284174625e
Christophe Bedard <bedard.christophe@gmail.com>
2018-10-07 15:34:15 -0400

    Add JS deploy script

---
+++ deploy.js
@@ -0,0 +1,21 @@
+var FtpDeploy = require('ftp-deploy');
+var ftpDeploy = new FtpDeploy();
+
+// Some environment variables have to be set separately (FTP_URL, FTP_USER, and FTP_PASSWORD)
+var config = {
+    user: process.env.FTP_USER,
+    password: process.env.FTP_PASSWORD,
+    host: process.env.FTP_URL,
+    port: 21,
+    localRoot: __dirname + '/src/',
+    remoteRoot: '/nova_html/',
+    include: ['*', '**/*'],
+    exclude: [],
+    deleteRemote: false
+}
+
+ftpDeploy.deploy(config)
+    .then(res => console.log('finished'))
+    .catch(err => console.log(err));
+
+ftpDeploy.on('uploading', data => console.log('uploaded', data));
