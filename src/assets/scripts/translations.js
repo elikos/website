720579541385c36c803a932203734e7dd92cb6c7
Geoffrey Leclaire <geoffrey.leclaire+github@outlook.com>
2018-10-14 13:31:44 -0400

    English translation done

--- src/assets/scripts/translations.js
+++ src/assets/scripts/translations.js
@@ -1,28 +1,45 @@
 $.ajaxSetup({
   beforeSend: function(xhr){
-    if (xhr.overrideMimeType)
-    {
+    if (xhr.overrideMimeType) {
       xhr.overrideMimeType("application/json");
     }
   }
 });
 
-$.getJSON("assets/locales/fr/translation.json", function(frTranslation) {
-  // use plugins and options as needed, for options, detail see
-  // http://i18next.com/docs/
-  i18next.init({
-    lng: 'fr', // evtl. use language-detector https://github.com/i18next/i18next-browser-languageDetector
-    resources: { // evtl. load via xhr https://github.com/i18next/i18next-xhr-backend
-      fr: {
-        translation: frTranslation
+$.getJSON("assets/locales/en/translation.json", function(enTranslation) {
+  $.getJSON("assets/locales/fr/translation.json", function(frTranslation) {
+    // use plugins and options as needed, for options, detail see
+    // http://i18next.com/docs/
+    i18next.init({
+      lng: 'fr', // evtl. use language-detector https://github.com/i18next/i18next-browser-languageDetector
+      fallbackLng: ['fr'],
+      preload: ['fr', 'en'],
+      resources: { // evtl. load via xhr https://github.com/i18next/i18next-xhr-backend
+        fr: {
+          translation: frTranslation
+        },
+        en: {
+          translation: enTranslation
+        }
       }
-    }
-  }, function(err, t) {
-    // for options see
-    // https://github.com/i18next/jquery-i18next#initialize-the-plugin
-    jqueryI18next.init(i18next, $);
-    // start localizing, details:
-    // https://github.com/i18next/jquery-i18next#usage-of-selector-function
-    $('body').localize();
+    }, function(err, t) {
+      // for options see
+      // https://github.com/i18next/jquery-i18next#initialize-the-plugin
+      jqueryI18next.init(i18next, $);
+      // start localizing, details:
+      // https://github.com/i18next/jquery-i18next#usage-of-selector-function
+      $('body').localize();
+
+      $('#change-language').click(() => {
+        let newLanguage = 'en';
+        if (i18next.language === 'en') {
+          newLanguage = 'fr';
+        }
+        i18next.changeLanguage(newLanguage);
+        $('body').localize();
+      });
+    });
   });
 });
+
+
